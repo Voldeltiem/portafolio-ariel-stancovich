663d6781b64cb3edbc3aa829464d4e803a63a9ba
Ariel Stancovich <arielstancovichsuarez@gmail.com>
2023-07-19 23:55:29 -0400

    NavBar 50%
    se suma NavBar solo base

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
     <>
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
